0d564aabb9b23e954e206163e1210f70f3bd45af
Aluno <ryancezarts@gmail.com>
2025-11-10 22:10:49 -0300

    chore: configura branch develop feat(login): criação inicial da classe de login chore(release): prepara v1.0 fix(readme): correção menor feat(validador): adiciona classe Validador com metodo validarEmail

---
+++ src/Validador.java
@@ -0,0 +1,9 @@
+public class Validador {
+    /**
+     * Método para validar o formato de um e-mail (simulação).
+     */
+    public void validarEmail() {
+        // No futuro, aqui entraria a lógica de validação (Regex)
+        System.out.println("Método validarEmail() executado.");
+    }
+}
